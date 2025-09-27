a7f99622919c9ff2ed056c4a491f40e74b2da83b
noliaphyta <201391998+ghostintheswem@users.noreply.github.com>
2025-09-27 13:27:38 -0400

    finished tuples

--- names.py
+++ names.py
@@ -2,15 +2,23 @@ from responses import importdata
 import pandas as pd
 from datetime import datetime, timedelta
 
-newnames = importdata()
-newnames['Timestamp'] = pd.to_datetime(newnames['Timestamp'])
+def getnames():
+    """calls import and returns tuples in list"""
 
-now = datetime.now()
-one_week_ago = now - timedelta(days=7)
-past_week_df = newnames[newnames['Timestamp'] >= one_week_ago]
-print(past_week_df)
+    newnames = importdata()
+    newnames['Timestamp'] = pd.to_datetime(newnames['Timestamp'])
 
-text_columns = newnames.iloc[:, [1,2]]
-for _, row in text_columns.iterrows():
-    print(row[0], row[1])
+    now = datetime.now()
+    one_week_ago = now - timedelta(days=7)
+    past_week_df = newnames[newnames['Timestamp'] >= one_week_ago]
+    print(past_week_df)
+
+    text_columns = newnames.iloc[:, [1,2]]
+
+    listnames = [tuple(row) for _, row in text_columns.iterrows()]
+    return listnames
+
+if __name__ == "__main__":
+    listnames = getnames()
+    print(listnames)
 
